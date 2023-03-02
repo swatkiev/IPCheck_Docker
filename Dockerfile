6002c096c5c993bbb0a7166e39ea0bf1f504912a
s-solovei <s.solovei@partner.samsung.com>
2023-03-02 15:29:58 +0200

    new changes

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3.9-alpine
 
-RUN pip install requests && pip install pyTelegramBotAPI
+RUN pip install requests && pip install pyTelegramBotAPI && pip install ipaddress
 
 WORKDIR /opt/ipbot
 
--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import requests
 import telebot
+import ipaddress
 
 BOT_TOKEN = 'PUT HERE YOUR TOKEN FROM BOT_FATHER'
 
@@ -10,32 +11,34 @@ def hello_handler(message):
     chat_id = message.chat.id
     bot.send_message(chat_id, 'Добрый день! Введите корректный IP адрес для получения информации')
 
+
 @bot.message_handler(content_types=['text'])
-def handler_text(message):
+def handler_text2(message):
     text = message.text
-    print('Text from user - ', text)
-
     try:
-        response = requests.get(url=f'http://ip-api.com/json/{text}').json()
-        # print(response)
+        test = ipaddress.ip_address(text)
+        if test.is_private == True:
+            bot.send_message(message.chat.id, 'Данный IP адрес не является внешним!')
+        else:
+            response = requests.get(url=f'http://ip-api.com/json/{text}').json()
+            # print(response)
 
-        data1 = str('IP ' + ': ' + response.get('query'))
-        data2 = str('Int prov ' + ': ' + response.get('isp'))
-        data3 = str('Org ' + ': ' + response.get('org'))
-        data4 = str('Country ' + ': ' + response.get('country'))
-        data5 = str('Region Name ' + ': ' + response.get('regionName'))
-        data6 = str('City ' + ': ' + response.get('city'))
-        data7 = str('ZIP ' + ': ' + response.get('zip'))
-        data8 = response.get('lat')
-        data88 = str('Lat ' + ': ') + str(data8)
-        data9 = response.get('lon')
-        data99 = str('Lon ' + ': ') + str(data9)
+            data1 = str('IP ' + ': ' + response.get('query'))
+            data2 = str('Int prov ' + ': ' + response.get('isp'))
+            data3 = str('Org ' + ': ' + response.get('org'))
+            data4 = str('Country ' + ': ' + response.get('country'))
+            data5 = str('Region Name ' + ': ' + response.get('regionName'))
+            data6 = str('City ' + ': ' + response.get('city'))
+            data7 = str('ZIP ' + ': ' + response.get('zip'))
+            data8 = response.get('lat')
+            data88 = str('Lat ' + ': ') + str(data8)
+            data9 = response.get('lon')
+            data99 = str('Lon ' + ': ') + str(data9)
 
-        result = str(data1+'\n'+data2+'\n'+data3+'\n'+data4+'\n'+data5+'\n'+data6+'\n'+data7+'\n'+data88+'\n'+data99)
+            result = str(data1+'\n'+data2+'\n'+data3+'\n'+data4+'\n'+data5+'\n'+data6+'\n'+data7+'\n'+data88+'\n'+data99)
 
-        bot.send_message(message.chat.id, result)
-    
-    except requests.exceptions.ConnectionError:
-        print('[!] Please check your connection!')
+            bot.send_message(message.chat.id, result)
+    except ValueError:
+        bot.send_message(message.chat.id, 'Некорректный IP адрес!')
 
 bot.polling()
